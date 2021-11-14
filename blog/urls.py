ffafdd877f5eec9f305b8f2e38e802104bd5eb4b
coffeblackpremium <joaodamaceno188@gmail.com>
2021-11-14 19:03:43 -0400

    Adicionado novas imagens no IMG, adicionado novos arquivos HTML para os Posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from blog import views
 
 urlpatterns = [
-    path('', views.home, name="home")
-
+    path('', views.home, name="home"),
+    path('helloworld/', views.helloworld, name="post_helloworld"),
+    path('seja_agua_amigo/', views.post_brucelee, name="post_brucelee"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,9 @@ from django.views.generic import ListView, DetailView
 
 def home(request):
     return render(request, 'partials/index.html')
+
+def helloworld(request):
+    return render(request, 'blog/posts/helloworld.html')
+
+def post_brucelee(request):
+    return render(request, 'blog/posts/02-post-bruce.html')
